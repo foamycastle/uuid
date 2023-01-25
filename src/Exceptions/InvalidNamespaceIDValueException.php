d08638402614afc359bd0d2f9ab03b8cf1a2de35
Aaron Sollman <74476626+foamycastle@users.noreply.github.com>
2023-01-25 07:32:42 -0600

    psr-4 restructure

--- src/Exceptions/InvalidNamespaceIDValueException.php
+++ src/Exceptions/InvalidNamespaceIDValueException.php
@@ -3,7 +3,7 @@
 namespace FoamyCastle\UUID\Exceptions;
 
 class InvalidNamespaceIDValueException extends \Exception {
-	function __construct(string $message = "", int $code = 0, ?Throwable $previous = null) {
+	function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null) {
 		parent::__construct("The given namespace ID is not valid");
 	}
 }
--- src/Exceptions/TimestampRaceConditionException.php
+++ src/Exceptions/TimestampRaceConditionException.php
@@ -3,7 +3,7 @@
 namespace FoamyCastle\UUID\Exceptions;
 
 class TimestampRaceConditionException extends \Exception {
-	function __construct($nodeID, string $message = "", int $code = 0, ?Throwable $previous = null) {
+	function __construct($nodeID, string $message = "", int $code = 0, ?\Throwable $previous = null) {
 		parent::__construct(
 			"A race condition for node ID '$nodeID' has occurred"
 		);
